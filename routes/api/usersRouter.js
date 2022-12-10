c9339d112ddeaf44f8f1816671a282714850dd4e
Haik <55463651+salampego@users.noreply.github.com>
2022-12-10 21:45:52 +0200

    done task

--- routes/api/usersRouter.js
+++ routes/api/usersRouter.js
@@ -5,6 +5,8 @@ const {
   getCurrent,
   logOut,
   setAvatar,
+  verification,
+  resendVerificationCode,
 } = require("../../controllers/userController");
 const asyncWrapper = require("../../helpers/apiHelpers");
 const { authMiddleware } = require("../../middlewares/authMiddlewares");
@@ -12,14 +14,22 @@ const { upload } = require("../../middlewares/upload");
 
 const router = express.Router();
 router.post("/register", asyncWrapper(registerController));
+
 router.get("/login", asyncWrapper(loginController));
+
 router.post("/logout", asyncWrapper(authMiddleware), asyncWrapper(logOut));
+
 router.get("/current", asyncWrapper(authMiddleware), asyncWrapper(getCurrent));
+
 router.patch(
   "/avatars",
   asyncWrapper(authMiddleware),
   asyncWrapper(upload.single("avatar")),
   asyncWrapper(setAvatar)
 );
+
+router.get("/verify/:verificationToken", verification);
+
+router.post("/verify", asyncWrapper(resendVerificationCode));
 
 module.exports = router;
